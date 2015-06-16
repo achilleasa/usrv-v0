7d1f3fd0f4cdaafbb9360cd0a53ca58f17b8ea54
Achilleas Anagnostopoulos <achilleas@taxibeat.com>
2015-06-16 23:48:16 +0300

    Include main package when running tests

--- test-coverage.sh
+++ test-coverage.sh
@@ -7,9 +7,18 @@
 set -e
 echo "" > coverage.txt
 
+# Main package
+go test -v -coverprofile=profile.out -covermode=atomic ./
+if [ -f profile.out ]; then
+    cat profile.out >> coverage.txt
+    echo '<<<<<< EOF' >> coverage.txt
+    rm profile.out
+fi
+
+# Sub-packages
 for d in $(find ./* -maxdepth 10 -type d); do
     if ls $d/*.go &> /dev/null; then
-        go test  -coverprofile=profile.out -covermode=atomic $d
+        go test -v -coverprofile=profile.out -covermode=atomic $d
         if [ -f profile.out ]; then
             cat profile.out >> coverage.txt
             echo '<<<<<< EOF' >> coverage.txt
